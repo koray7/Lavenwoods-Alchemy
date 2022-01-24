b79c8d0dd9a0aa8406e6dd9250d3b00576a46919
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-01-24 11:39:58 -0600

    basic express app running

---
+++ app.js
@@ -0,0 +1,26 @@
+
+//DEPENDENCIES
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+//MIDDLEWARE
+
+
+//ROUTING
+
+
+
+
+
+
+
+
+
+
+
+
+//SERVER START
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
