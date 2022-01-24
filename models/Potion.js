4c19854070d3fe0ffce8b5463bf9c16a9adcf336
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-01-24 13:00:33 -0600

    added main potion schema

--- models/Potion.js
+++ models/Potion.js
@@ -1,1 +1,8 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+
+const potionSchema = new mongoose.Schema({
+    name: {
+        type: String, required: true, default: "Unidentified Brew"
+    },
+    attributes: [String]
+}, {timestamps: true})
