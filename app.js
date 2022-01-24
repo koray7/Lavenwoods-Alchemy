5370c80183347cf9aecff6a37949827e11d86d3f
Selena Renteria <selena.renteria18@gmail.com>
2022-01-24 12:14:14 -0600

    merging branch info

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+
+// route for create page - create potion 
+
+
+//show page to show 1 potion that was created 
+
+
+
+// start the server 
+app.listen(PORT, () => {
+    console.log(`server is started on port ${PORT}`)
+})
