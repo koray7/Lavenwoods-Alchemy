4e1f17000c1639f08da7db442035b56512f9b451
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-01-25 16:10:42 -0600

    created mage.js model and added object containing mages and their attributes

--- controllers/mages.js
+++ controllers/mages.js
@@ -9,4 +9,15 @@ router.get('/', (req, res) => {
     res.render('mages/mages.ejs')
 })
 
+//show
+router.get('/:id', (req, res) => {
+    Potion.findById(req.params.id, (err, foundMage) => {
+        if(err) {
+            res.send(err)
+        } else {
+            res.render('mages/show.ejs', {mage: foundMage})
+        }
+    })
+})
+
 module.exports = router
