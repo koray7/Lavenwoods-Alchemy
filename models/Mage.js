581fa8fc68a76afb7bb7c6d85b85a20941f908b1
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-01-26 10:01:25 -0800

    repushing

--- models/Mage.js
+++ models/Mage.js
@@ -1,27 +1,27 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const Mages = {
-//     mageFlame: {
-//         name: "Mage O' Flame",
-//         element: "Fire",
-//         quote: "",
-//     },
-//     mageBolt: {
-//         name: "Mage O' Bolt",
-//         element: "Lightning",
-//         quote: ""
-//     },
-//     mageFrost: {
-//         name: "Mage O' Frost",
-//         element: "Ice",
-//         quote: "",
-//     },
-//     mageEnigma: {
-//         name: "Mage O' Enigma",
-//         element: "Mystery",
-//         quote: "",
-//     }
-// }
+const Mages = {
+    mageFlame: {
+        name: "Mage O' Flame",
+        element: "Fire",
+        quote: "",
+    },
+    mageBolt: {
+        name: "Mage O' Bolt",
+        element: "Lightning",
+        quote: ""
+    },
+    mageFrost: {
+        name: "Mage O' Frost",
+        element: "Ice",
+        quote: "",
+    },
+    mageEnigma: {
+        name: "Mage O' Enigma",
+        element: "Mystery",
+        quote: "",
+    }
+}
 
-// const Mage = mongoose.model("Mage", Mages)
-// module.exports = Mage
+const Mage = mongoose.model("Mage", Mages)
+module.exports = Mage
