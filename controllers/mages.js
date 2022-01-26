9a8119bfb80f0f7276e4d7ccff31bcae2f039e07
Selena Renteria <selena.renteria18@gmail.com>
2022-01-26 14:04:25 -0600

    rearranged my models mages data

--- controllers/mages.js
+++ controllers/mages.js
@@ -1,55 +1,36 @@
 const express = require('express')
 const router = express.Router()
-const Mages = [
-    mageFlame = {
-        name: "Mage O' Flame",
-        element: "Fire",
-        quote: "",
-        image: "https://i.ibb.co/64LzfMQ/Flame-Mage.png",
-    },
-    mageBolt = {
-        name: "Mage O' Bolt",
-        element: "Lightning",
-        quote: "",
-        image: "https://i.ibb.co/72TQd3j/Bolt-Mage.png",
-    },
-    mageFrost = {
-        name: "Mage O' Frost",
-        element: "Ice",
-        quote: "",
-        image: "https://i.ibb.co/DVTHvCP/Frost-Mage.png",
-    },
-    mageEnigma = {
-        name: "Mage O' Enigma",
-        element: "Mystery",
-        quote: "",
-        image: "https://i.ibb.co/zFsHZXn/Enigma-Mage.png",
-    }
-]
+const Mages = require('../models/Mage')
+
+
+
+
 
 //ROUTES
 
 //index
 router.get('/', (req, res) => {
-    res.render('mages/mages.ejs', {mage: Mages})
-    // Mage.find({}, (err, allMages) => {
-    //     if (err){
-    //         res.send(err)
-    //     } else {
-    //         res.render('mages/mages.ejs', {mage: allMages})
-    //     }
-    // })
+    // res.render('mages/mages.ejs', {mage: Mages})
+    Mages.find({}, (err, allMages) => {
+        if (err){
+            res.send(err)
+        } else {
+            res.render('mages/mages.ejs', {mage: allMages})
+        }
+    })
 })
 
 //show
 router.get('/:id', (req, res) => {
-    Mage.findById(req.params.id, (err, foundMage) => {
+    Mages.findById(req.params.id, (err, foundMage) => {
         if(err) {
             res.send(err)
+            console.log("Am I working?")
         } else {
             res.render('mages/show.ejs', {mage: foundMage})
         }
     })
 })
+
 
 module.exports = router
