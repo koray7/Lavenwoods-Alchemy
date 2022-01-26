4087d7e9afaa0a71873be8f26bffe1eedabc8b7b
srenteria28 <95322104+srenteria28@users.noreply.github.com>
2022-01-26 10:15:08 -0600

    Update Mage.js
    changed the rest of the potions to Mage 🧙‍♀️

--- models/Mage.js
+++ models/Mage.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const potionSchema = new mongoose.Schema({
+const mageSchema = new mongoose.Schema({
     mageName: {
         type: String, required: true, default: "Unidentified Mage"
     },
@@ -9,5 +9,5 @@ const potionSchema = new mongoose.Schema({
     image: String
     }, {timestamps: true})
 
-const Potion = mongoose.model("Potion", potionSchema)
-module.exports = Potion
+const Mage = mongoose.model("Mage", mageSchema)
+module.exports = Mage
