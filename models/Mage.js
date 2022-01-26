b3bc1b2c0cda10abec685b14c772e044a23939e0
arir2020 <ari.richards98@gmail.com>
2022-01-26 15:13:40 -0500

    safety save

--- models/Mage.js
+++ models/Mage.js
@@ -1,6 +1,38 @@
 const mongoose = require('mongoose')
 
-const potionSchema = new mongoose.Schema({
+const Mages = [
+    mageFlame = {
+        _id: 1,
+        name: "Mage O' Flame",
+        element: "Fire",
+        quote: "",
+        image: "https://i.ibb.co/64LzfMQ/Flame-Mage.png",
+    },
+    mageBolt = {
+        _id: 2,
+        name: "Mage O' Bolt",
+        element: "Lightning",
+        quote: "",
+        image: "https://i.ibb.co/72TQd3j/Bolt-Mage.png",
+    },
+    mageFrost = {
+        _id: 3,
+        name: "Mage O' Frost",
+        element: "Ice",
+        quote: "",
+        image: "https://i.ibb.co/DVTHvCP/Frost-Mage.png",
+    },
+    mageEnigma = {
+        _id: 4,
+        name: "Mage O' Enigma",
+        element: "Mystery",
+        quote: "",
+        image: "https://i.ibb.co/zFsHZXn/Enigma-Mage.png",
+    }
+]
+
+
+const mageSchema = new mongoose.Schema({
     mageName: {
         type: String, required: true, default: "Unidentified Mage"
     },
@@ -9,5 +41,5 @@ const potionSchema = new mongoose.Schema({
     image: String
     }, {timestamps: true})
 
-const Potion = mongoose.model("Potion", potionSchema)
-module.exports = Potion
+const Mage = mongoose.model("Mage", mageSchema)
+module.exports = Mage, Mages
