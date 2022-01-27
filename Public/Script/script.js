a30fdce418f1a29c2b82801165fc6a08e9d96c91
Travis Thompson <96032956+ttguitarnoob-dev@users.noreply.github.com>
2022-01-27 14:34:34 -0600

    added api fetch for random fact generator

--- Public/Script/script.js
+++ Public/Script/script.js
@@ -1,1 +1,30 @@
 console.log("is it working")
+
+let url = "https://uselessfacts.jsph.pl/random.json?language=en"
+
+const options = {
+    method: 'GET',
+    headers: {},
+}
+
+let mageQuote = ""
+
+// const mageClicker = document.getElementsByClassName('mage-clicker')
+// mageClicker.addEventListener('click', getQuote)
+
+function getQuote(){
+    fetch(url, options).then((resp) => {
+        resp.json().then((data) =>{
+            mageQuote = data.text
+            const display = document.getElementById('mage-quote')
+            display.innerText = mageQuote
+            })
+        })
+    }
+
+    // window.onload = function() {
+    //   getQuote()
+       
+    // }
+
+    // module.exports = mageQuote
