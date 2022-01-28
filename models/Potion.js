59e3903f973ffeb693c794c1babb2fa2e6d11f48
Selena Renteria <selena.renteria18@gmail.com>
2022-01-28 14:53:38 -0600

    erased some unwanted code

--- models/Potion.js
+++ models/Potion.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose')
 
 const potionSchema = new mongoose.Schema({
     potionName: {
-        type: String, required: true, default: "Unidentified Brew"
+        type: String, required: true, default:"Unidentified Brew"
+        
     },
     attributes: [String],
     mage: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Mage'
+    
     }
-    // type of mage/element string 
+   
 }, {timestamps: true})
 
 const Potion = mongoose.model("Potion", potionSchema)
